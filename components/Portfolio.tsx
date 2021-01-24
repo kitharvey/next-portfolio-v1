0d04df5467989e87c58ea3ac5a99eab9b3b6fcf2
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-01-25 05:18:11 +0800

    deploy to vercel

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -3,14 +3,12 @@ import Nav from './Nav'
 import Home from './Home'
 import Projects from './Projects'
 import About from './About'
-import Preloader from './Preloader'
 import Footer from './Footer'
 
 export default function Portfolio() {
     
     return (
         <div className="wrapper" > 
-            <Preloader></Preloader>
             <Nav></Nav>
             <Home></Home>
             <Projects></Projects>
