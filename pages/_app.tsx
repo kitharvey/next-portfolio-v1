32af09d7532223f021d998b893bf56a69de788b8
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-01-31 22:47:42 +0800

    _app fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,8 @@
 
 import React from 'react'
-
 import '../scss/style.scss'
-import Portfolio from '../components/Portfolio'
-import Head from 'next/head'
-export default function MyApp() {
-  return (
-    <>
-        <Head>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <title>Kit Harvey | Front-End Dev</title>
-            <link rel="shortcut icon" type="image/png" href="/icon.png" sizes="50x32"/>
-            <meta name="title" content="Kit Harvey" />
-            <meta
-              name="description"
-              content="I'm a self-taught front-end developer. I help clients and companies build their web user interfaces using React, TypeScript and Sass."
-            />
-        </Head>
-        <Portfolio/>
-    </>
-  )
+import type { AppProps } from 'next/app'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
-// import MyApp from './_app'
-
+import Portfolio from '../components/Portfolio'
+import Head from 'next/head'
 const IndexPage = () => (
-    <></>
+    <>
+    <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Kit Harvey | Front-End Dev</title>
+        <link rel="shortcut icon" type="image/png" href="/icon.png" sizes="50x32"/>
+        <meta name="title" content="Kit Harvey" />
+        <meta
+          name="description"
+          content="I'm a self-taught front-end developer. I help clients and companies build their web user interfaces using React, TypeScript and Sass."
+        />
+    </Head>
+    <Portfolio/>
+</>
 )
 
 export default IndexPage
