bd78dc723ad7acc4404932da532ca44305482337
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-03-02 22:12:56 +0800

    meta tags updated

--- components/HeadTitle.tsx
+++ components/HeadTitle.tsx
@@ -5,6 +5,9 @@ interface HeadTitleProps {
     title: string
 }
 
+const metaIMG = "/meta-img.PNG"
+const metaDescription = "I'm a self-taught front-end developer. I help clients and companies build their web user interfaces using ReactJS, NextJS, TypeScript and Sass."
+
 const HeadTitle: React.FC<HeadTitleProps> = ({title}) => {
         return (
             <Head>
@@ -14,8 +17,30 @@ const HeadTitle: React.FC<HeadTitleProps> = ({title}) => {
                 <link rel="shortcut icon" type="image/png" href="/icon.png" sizes="50x32"/>
                 <meta name="title" content="Kit Harvey" />
                 <meta
-                name="description"
-                content="I'm a self-taught front-end developer. I help clients and companies build their web user interfaces using React, TypeScript and Sass."
+                    name="description"
+                    content={metaDescription}
+                />
+                <meta
+                    name="image"
+                    content={metaIMG}
+                />
+                <meta name="og:title" content="Kit Harvey" />
+                <meta
+                    name="og:description"
+                    content={metaDescription}
+                />
+                <meta
+                    name="og:image"
+                    content={metaIMG}
+                />
+                <meta name="twitter:title" content="Kit Harvey" />
+                <meta
+                    name="twitter:description"
+                    content={metaDescription}
+                />
+                <meta
+                    name="twitter:image"
+                    content={metaIMG}
                 />
             </Head>
         );
